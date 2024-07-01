1f1ebd9d1912a85cbaab7973b0613ffef021ccbd
Ethan <thegamershollow@outlook.com>
2024-07-01 20:23:41 +0000

    created world 4 assets

--- Assets/4-Bit Side Scroller Tiles.tsx
+++ Assets/4-Bit Side Scroller Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="4-Bit Side Scroller Tiles" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.11.0" name="4-Bit Side Scroller Tiles" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="4-Bit Side Scroller Tiles.png" width="512" height="512"/>
 </tileset>

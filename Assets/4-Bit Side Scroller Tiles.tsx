1bdf5b0cfc7d67428038d18d233c4bb7f7c023b6
Ethan <thegamershollow@outlook.com>
2024-07-01 10:21:32 +0000

    added tiled assets

---
+++ Assets/4-Bit Side Scroller Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="4-Bit Side Scroller Tiles" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../../../Downloads/4-Bit Side Scroller Tiles.png" width="512" height="512"/>
+</tileset>
